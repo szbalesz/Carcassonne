9359df6795bcacc0ece6960bbb7a78c9f708a244
SzBalesz <baleszvagyok7000@gmail.com>
2023-03-24 09:36:34 +0100

    Igen

---
+++ jatek.js
@@ -0,0 +1,10 @@
+
+
+function kezdes(){
+var jatekter = document.getElementById("jatekter");
+document.getElementById("kezdes").remove();
+
+}
+
+
+document.getElementById("kezdes").addEventListener('click',kezdes);
